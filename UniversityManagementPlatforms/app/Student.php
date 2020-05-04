a3b8432064fcfd0027357f095d8d50815738b568
Amine <aminebenslama10@gmail.com>
2020-05-04 15:40:24 +0100

    relation

--- UniversityManagementPlatforms/app/Student.php
+++ UniversityManagementPlatforms/app/Student.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    public function student_attendances()
+    {
+        return $this->hasMany('App\StudentAttendance');
+    }
+    
+    public function classes()
+    {
+        return $this->belongsTo('App\Classe');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
