809667477783de055b3181ba0315489f9c53f55a
malekbk98 <47837824+malekbk98@users.noreply.github.com>
2020-06-13 14:59:08 +0100

    Teacher Dashboard stat

--- UniversityManagementPlatforms/app/Http/Controllers/HomeController.php
+++ UniversityManagementPlatforms/app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ use App\Department;
 use App\Classe;
 use App\User;
 use App\Subject;
+use App\Student;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -34,12 +35,26 @@ class HomeController extends Controller
             $stat['department'] = count(Department::all());
             $stat['student'] = count(User::where('position','=','student')->get());
             $stat['teacher'] = count(User::where('position','=','teacher')->get());
-
-
             return view('admin.dashboard',compact('stat'));
         }
-        else{
-            return view('home');
+        elseif(auth::user()->position=='teacher'){
+
+            $classatt =auth::user()->teachers()
+            ->join ('subjects','teacher_id','=','teachers.id')
+            ->join('lessons', 'subject_id', '=', 'subjects.id')
+            ->join('classes', 'classes.id', '=', 'lessons.classe_id')->distinct('classes.classe_name')->get();
+            $student_nbr=0;
+            foreach($classatt as $class){
+                $classroom =Student::where('classe_id','=',$class->id)
+                ->join('users','users.id','=','students.user_id')
+                ->get();
+                foreach($classroom as $students){
+                    $student_nbr++;
+                }
+            }
+            $stat['classe'] = count($classatt);
+            $stat['student'] = $student_nbr;
+            return view('teacher_attendance.dashboard',compact('stat'));
         }
     }
 }
---
+++ UniversityManagementPlatforms/resources/views/teacher_attendance/dashboard.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.Menu')
+
+@section('content')
+<br>
+<section class="content">
+    <div class="container-fluid">
+      <!-- Small Box (Stat card) -->
+      <div class="row">
+        <div class="col-lg-4 col-6">
+          <!-- small card -->
+          <div class="small-box bg-info">
+            <div class="inner">
+              <h3>{{$stat['student']}}</h3>
+              <p>Students</p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-user-graduate"></i>
+            </div>
+            <a href="{{route('classelist')}}" class="small-box-footer">
+              More info <i class="fas fa-arrow-circle-right"></i>
+            </a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-4 col-6">
+            <!-- small card -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>{{$stat['classe']}}</h3>
+                <p>Classes</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-chalkboard-teacher"></i>
+              </div>
+              <a href="{{route('classelist')}}" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+              </a>
+            </div>
+          </div>
+          <div class="col-lg-4 col-6">
+            <!-- small card -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>{{$stat['classe']}}</h3>
+                <p>Subjects</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-flask"></i>
+              </div>
+              <a href="{{route('classelist')}}" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+              </a>
+            </div>
+          </div>
+
+      </div>
+</section>
+@endsection
